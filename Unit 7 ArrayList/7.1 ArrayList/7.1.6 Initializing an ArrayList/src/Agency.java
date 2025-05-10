9f3a1f7943743184b2e1a96f4a10a8c9bd7cfd52
Mal <mariuscastillo0512@gmail.com>
2025-05-10 16:38:00 -0400

    7.1.65 Initializing an ArrayList

---
+++ Unit 7 ArrayList/7.1 ArrayList/7.1.6 Initializing an ArrayList/src/Agency.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Agency
+{
+    public static void main(String[] args)
+    {
+        //Initialize your ArrayLists here!
+        ArrayList <String> contactName = new ArrayList <String>();
+
+        ArrayList <Double> hourlyRate = new ArrayList <Double>();
+        
+    }
+}
