002a5d9520f54dff7672b4a62ff0301d82dd5730
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 09:52:26 -0400

    5.1.4 Access for DNA Class

---
+++ Unit 5 Writing Classes/5.1 Writing Classes/5.1.4 Access for DNA Class/src/DNAObject.java
@@ -0,0 +1,11 @@
+public class DNAObject
+{
+    private String snp = "";
+    private String genotype = "";
+
+    public DNAObject(String one, String two)
+    {
+        snp = one;
+        genotype = two;
+    }
+}
