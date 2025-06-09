e385c0443c065010891825f32d75b21f86ff34e1
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 10:02:51 -0400

    5.2.7 Student Overload

---
+++ Unit 5 Writing Classes/5.2 Constructors/5.2.7 Student Overload/src/Student.java
@@ -0,0 +1,50 @@
+public class Student
+{
+    private String firstName;
+    private String lastName;
+    private int gradeLevel;
+    private String school;
+
+    /**
+     * Constructors go here.
+     * Check out StudentTester.java for an example of how to use
+     * this constructor. Make sure your code matches the call in the
+     * tester.
+     */
+    public Student(String first, String last, int grade, String schoolType)
+    {
+        firstName = first;
+        lastName = last;
+        gradeLevel = grade;
+        school = schoolType;
+    }
+
+    public Student(String first, String last, int grade)
+    {
+        firstName = first;
+        lastName = last;
+        gradeLevel = grade;
+        if (grade<6)
+            school = "elementary school";
+        else if (grade<9)
+            school = "middle school";
+        else
+            school = "high school";
+    }
+
+    /**
+     * This is a toString for the Student class. It returns a String
+     * representation of the object, which includes the fields
+     * in that object.
+     * 
+     * Modify the to string to add 'and goes to _____' at the end
+     */
+    public String toString()
+    {
+        return firstName + " " + lastName + " is in grade " + gradeLevel + " and goes to " + school;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+}
