2cf20a79d3081ed2b852bb3f7383dea2bbcfed01
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 10:11:57 -0400

    5.3.7 C.Y.O.A. Layout

---
+++ Unit 5 Writing Classes/5.3 Documentation with Comments/5.3.7 C.Y.O.A. Layout/src/CYOA.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CYOA
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+
+        //Introduction
+
+        //Name Prompt
+
+        //Greeting
+
+        //First Choice Prompt: Mike Tyson or Jake Paul?
+            //User Chooses Mike Tyson
+                //Mike Tyson Description
+            //User User Chooses Jake Paul
+                //Jake Paul Description
+
+        //Second Choice Prompt: Vanilla or Chocolate?
+            //User Chooses Vanilla
+                //Vanilla Description
+            //User Chooses Chocolate
+                //Chocolate Description
+
+        //Ending One: Mike Tyson and Vanilla
+
+        //Ending Two: Mike Tyson and Chocolate
+        
+        //Ending Three: Jake Paul and Vanilla
+
+        //Ending Four: Jake Paul and Chocolate
+    }
+}
