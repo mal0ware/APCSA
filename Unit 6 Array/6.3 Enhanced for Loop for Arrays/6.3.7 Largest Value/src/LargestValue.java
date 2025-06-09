887c39cb67c53ee272645daaf77a8a8b6a803239
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 09:33:34 -0400

    6.3.7 Largest Value

---
+++ Unit 6 Array/6.3 Enhanced for Loop for Arrays/6.3.7 Largest Value/src/LargestValue.java
@@ -0,0 +1,34 @@
+public class LargestValue     
+{ 
+    public static void main(String[] arg) 
+    { 
+        { 
+            int[] values = {32, 56, 79, 2, 150, 37}; 
+              
+            int highestValue = findMax(values); 
+            System.out.println("The highest score is " + highestValue); 
+        } 
+    } 
+    public static int findMax(int[] numbers) 
+    {  
+        int maxSoFar = numbers[0];
+        /*
+        int i = 0;
+        while (i < numbers.length){
+            if (numbers[i] > maxSoFar) 
+            { 
+                maxSoFar = numbers[i]; 
+            }
+            i ++;
+        }
+        */
+        for (int num : numbers)
+        {
+            if (num > maxSoFar)
+            {
+                maxSoFar = num;
+            }
+        }
+    return maxSoFar; 
+    } 
+}
