6dc6d57ebaf026ed7661ae03206c9c5273a4d2f9
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 10:01:03 -0400

    5.2.6 Dog Class

---
+++ Unit 5 Writing Classes/5.2 Constructors/5.2.6 Dog Class/src/DogTester.java
@@ -0,0 +1,12 @@
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dog dog1 = new Dog("Doggy", 7, 14.5);
+        Dog dog2 = new Dog("Mr. Pickles", 3);
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+    }
+}
