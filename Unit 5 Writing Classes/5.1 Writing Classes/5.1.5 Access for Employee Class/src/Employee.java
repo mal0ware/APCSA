d90403e7bc9c613b37947a2e5db039c3888a2ed2
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 09:53:46 -0400

    5.1.5 Access for Employee Class

---
+++ Unit 5 Writing Classes/5.1 Writing Classes/5.1.5 Access for Employee Class/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee
+{
+    private String name;
+    private int id;
+    private double salary;
+    public Employee(String employeeName, int idNum, double hourlySalary)
+    {
+        name = employeeName;
+        id = idNum;
+        salary = hourlySalary;
+    }
+}
