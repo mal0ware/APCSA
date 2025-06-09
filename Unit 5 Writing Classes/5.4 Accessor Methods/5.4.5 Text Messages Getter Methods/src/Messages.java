40c66e2945abb43073ae57038bd4ba437e4b4cd7
Marius Castillo <mariuscastillo0512@gmail.com>
2025-06-09 10:27:47 -0400

    5.4.5 Text Messages Getter Methods

---
+++ Unit 5 Writing Classes/5.4 Accessor Methods/5.4.5 Text Messages Getter Methods/src/Messages.java
@@ -0,0 +1,12 @@
+public class Messages
+{
+    public static void main(String[] args)
+    {
+        // Your code here.
+        // Create two TextMessage objects and print them out.
+        TextMessage First = new TextMessage("Marius", "Louis", "Skibidi?");
+        TextMessage Second = new TextMessage("Matthew", "Louis", "Hawk?");
+        System.out.println(First);
+        System.out.println(Second);
+    }
+}
