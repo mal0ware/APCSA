39f5c3bb2f8ebfd796ac454bed42e4704ce9af29
Mal <mariuscastillo0512@gmail.com>
2025-06-11 21:42:34 -0400

    5.4.

---
+++ Unit 5 Writing Classes/5.4 Accessor Methods/5.4.7 A Different Dragon Class/src/Dragon.java
@@ -0,0 +1,57 @@
+public class Dragon 
+{
+    private String name;
+    private int level;
+    private boolean canBreatheFire;
+    
+    // Write the constructor here!
+    public Dragon(String theName, int theLevel)
+    {
+        name = theName;
+        level = theLevel;
+        if(level>=50)
+            canBreatheFire = true;
+        else
+            canBreatheFire = false;
+    }
+    
+    // Put getters here
+    public String getName()
+    {
+        return name;
+    }
+    public int getLevel()
+    {
+        return level;
+    }
+    public boolean isFireBreather()
+    {
+        return canBreatheFire;
+    }
+
+    // Put other methods here
+    public void attack()
+    {
+        if(canBreatheFire)
+        {
+            System.out.println(">>>>>>");
+            System.out.println(">>>>>>>>>>");
+            System.out.println(">>>>>>");
+        }
+        else
+            System.out.println("* ~ ~ ~");
+    }
+    public void gainExperience()
+    {
+        level += 5;
+        if(level>=50)
+            canBreatheFire = true;
+        else
+            canBreatheFire = false;
+    }
+    // String representation of the object
+    public String toString()
+    {
+        return "Dragon " + name + " is at level " + level;
+    }
+}
---
+++ Unit 5 Writing Classes/5.4 Accessor Methods/5.4.7 A Different Dragon Class/src/DragonTester.java
@@ -0,0 +1,10 @@
+public class DragonTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dragon jimmy = new Dragon("Jimmy", 100);
+        jimmy.attack();
+        System.out.println(jimmy);
+    }
+}
